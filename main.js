8d5a00227133ff8c20b274a039545f10bfc358e3
narayan shrestha <benarayanz@gmail.com>
2022-06-10 16:00:31 +0545

    initial setup for gcloud deployment for helloworld project

---
+++ main.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/users', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      res.end( data );
+   });
+})
+
+const PORT = process.env.PORT || 8080; 
+
+var server = app.listen(PORT, function () {
+   var port = server.address().port
+   console.log("Example app listening at port: %s", port)
+})
